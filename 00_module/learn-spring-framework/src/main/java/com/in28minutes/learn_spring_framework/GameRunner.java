5a00dbf35b85507ac0ecccc3b35ffe2bc757718d
EH13 <philomonx1@gmail.com>
2024-06-18 15:32:17 +0900

    Feat : 4단계 - Java 게이밍 애플리케이션 시작하기
    *feat : `MarioGame` 마리오 게임 구현
    *feat : `GameRunner` 게임 실행 클래스 구현
    *feat : `AppGamingBasicJava` 게임 애플리케이션 구현

---
+++ 00_module/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/GameRunner.java
@@ -0,0 +1,18 @@
+package com.in28minutes.learn_spring_framework;
+
+public class GameRunner {
+
+	MarioGame game;
+
+	public GameRunner(MarioGame game) {
+		this.game = game;
+	}
+
+	public void run() {
+		System.out.println("게임 시작 : " + game);
+		game.up();
+		game.down();
+		game.left();
+		game.right();
+	}
+}
---
+++ 00_module/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/MarioGame.java
@@ -0,0 +1,17 @@
+package com.in28minutes.learn_spring_framework;
+
+public class MarioGame {
+	public void up(){
+		System.out.println("점프");
+	}
+
+	public void down(){
+		System.out.println("파이프로 이동");
+	}
+	public void left(){
+		System.out.println("뒤로 이동");
+	}
+	public void right(){
+		System.out.println("가속");
+	}
+}
