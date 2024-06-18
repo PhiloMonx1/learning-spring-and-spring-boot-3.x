3e8dcf2f39bc557481c73629212411221c554318
EH13 <philomonx1@gmail.com>
2024-06-18 18:47:15 +0900

    Chore : 애플리케이션 파일 이름 및 위치 변경 & 불필요 파일 삭제

--- 00_module/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App01GamingBasicJava.java
+++ 00_module/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App01GamingBasicJava.java
@@ -1,6 +1,9 @@
-package com.in28minutes.learn_spring_framework.game;
+package com.in28minutes.learn_spring_framework;
 
-public class AppGamingBasicJava {
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.PacmanGame;
+
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 
