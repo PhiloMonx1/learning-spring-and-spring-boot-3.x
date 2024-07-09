de24e4d7770b2a0d1ce527d85a83a263cb2e25fb
EH13 <philomonx1@gmail.com>
2024-07-09 21:27:50 +0900

    3단계 - 첫 코드에서 첫 단위 테스트 수행하기
    *feat : 테스트 코드 리팩토링 실습
    *docs : 테스트 코드 리팩토링 실습 & 좋은 테스트 코드의 조건 노트

--- 00_module/junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
+++ 00_module/junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
@@ -2,15 +2,28 @@ package com.in28minutes.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
 
-	@Test
-	void calculateSum() {
-		MyMath myMath = new MyMath();
-		int sum = myMath.calculateSum(new int[] { 1, 2, 3 });
+	private MyMath myMath;
 
-		assertEquals(9, sum);
+	@BeforeEach
+	void setUp() {
+		myMath = new MyMath();
+	}
+
+	@Test
+	@DisplayName("배열 내의 3개의 숫자를 합산하여 리턴합니다.")
+	void calculateSum_ThreeMembers() {
+		assertEquals(6, myMath.calculateSum(new int[]{1, 2, 3}));
+	}
+
+	@Test
+	@DisplayName("빈 배열이라면 0을 리턴합니다.")
+	void calculateSum_ZeroMember() {
+		assertEquals(0, myMath.calculateSum(new int[]{}));
 	}
 }
